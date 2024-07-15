b3afe38baff3b9bb28290c9348e2ad9bdb4a2196
ruzibekov <ruzibekov44@gmail.com>
2024-07-15 18:43:07 +0500

    2-masala

--- src/Main.kt
+++ src/Main.kt
@@ -1,33 +1,3 @@
 fun main() {
-    val list = listOf(9, 2, 5, 3, 1, 5, 3)
-    val sortedList = bubbleSort(list)
-    println(sortedList)
+    // Console-da 1qatorda vergul bilan ajratib, hohlagancha son kiritish mumkin. Kiritilgan barcha juft sonlarni yig'indisini toping
 }
-
-fun bubbleSort(inputList: List<Int>): List<Int> {
-    // Создаем изменяемый список из входного списка, чтобы можно было изменять элементы
-    val list = inputList.toMutableList()
-
-    // Получаем размер списка
-    val n = list.size
-
-    // Внешний цикл, который проходит по всему списку
-    for (i in 0 until n - 1) {
-        // Внутренний цикл, который проходит по списку до последнего неотсортированного элемента
-        for (j in 0 until n - 1 - i) {
-            // Сравниваем текущий элемент с следующим
-            if (list[j] > list[j + 1]) {
-                // Меняем их местами, если текущий элемент больше следующего
-                val temp = list[j]
-                list[j] = list[j + 1]
-                list[j + 1] = temp
-            }
-        }
-    }
-
-    // Возвращаем отсортированный список
-    return list
-}
-
-//val sortedList =list.sorted()
-//println(sortedList )
